d3f364bb8100a93c36a5c4702032b222e5f1d472
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-12-21 17:19:59 +0900

    ui refator

--- packages/ui-mode/BambooTheme.ts
+++ packages/ui-mode/BambooTheme.ts
@@ -1,13 +1,13 @@
 import { ColorThemeType } from "./type";
 
-export const light: ColorThemeType = {
+export const light = {
   white: "#fff",
   black: "#000",
   whiteInText: "#fff",
   blackInText: "#000",
 };
 
-export const dark: ColorThemeType = {
+export const dark = {
   white: "#000",
   black: "#fff",
   whiteInText: "#000",
