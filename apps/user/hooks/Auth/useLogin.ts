3469a1e2ea8efbc3d360be1f700772f25b580bd1
baekseungha <101560680+BaekSeungHa@users.noreply.github.com>
2023-02-28 20:28:34 +0900

    cookie -> local 변경

--- apps/user/hooks/Auth/useLogin.ts
+++ apps/user/hooks/Auth/useLogin.ts
@@ -1,20 +1,22 @@
-import { setToken } from "custom-util";
+import { setToken } from "../../util/localstorage";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { useEffect } from "react";
+import { Cookies } from "react-cookie";
 import authRepository from "../../repository/Auth/auth.repository";
 
 const useLogin = () => {
   const router = useRouter();
   const query = router.query;
+  const cookies = new Cookies();
 
   const request = useCallback(async () => {
     try {
       const { token, refreshToken } = await authRepository.login({
         code: query.code as string,
       });
-      setToken("accessToken", token);
-      setToken("refreshToken", refreshToken);
+      setToken(token, refreshToken);
+
       router.push("/");
     } catch (error) {
       console.log(error);
---
+++ apps/user/util/config.ts
@@ -0,0 +1,7 @@
+/**
+ * @todo baseUrl 작성
+ */
+export const conf = {
+  baseUrl: "http://13.209.33.18:8080",
+  clientId: "ddc27157c4684a2bb8bfe103ca692ee360f16d2afdee437fa065aa536658a199",
+};
